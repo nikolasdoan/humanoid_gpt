d0d8d082cb074eab896f5a82b1c0779ec144c676
Nikolas Doan <hoangphuongbluesky@gmail.com>
2024-10-10 00:57:22 +0800

    add shutdown handler for server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-# server.py
 import socket
+import signal
+import sys
 from gpt import run_gpt
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
@@ -15,24 +16,36 @@ server_socket.bind(server_address)
 server_socket.listen(1)
 print("Server is listening on port 5000...")
 
-while True:
-    # Wait for a connection
-    connection, client_address = server_socket.accept()
-    try:
-        print(f"Connection from {client_address}")
+def signal_handler(sig, frame):
+    print("Interrupt received, shutting down...")
+    server_socket.close()
+    sys.exit(0)
 
-        while True:
-            # Receive data from the client
-            data = connection.recv(1024)
-            if data:
-                received_text = data.decode()
-                print(f"Received: {received_text}")
-                if received_text == 'start':  # Replace 'start' with the specific character or string you want
-                    control_number = run_gpt()
-                    connection.sendall(str(control_number).encode()) # Send control_number back to the client
+# Register the signal handler for graceful shutdown
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+try:
+    while True:
+        # Wait for a connection
+        connection, client_address = server_socket.accept()
+        try:
+            print(f"Connection from {client_address}")
+
+            while True:
+                # Receive data from the client
+                data = connection.recv(1024)
+                if data:
+                    received_text = data.decode()
+                    print(f"Received: {received_text}")
+                    if received_text == 'start':  # Replace 'start' with the specific character or string you want
+                        control_number = run_gpt()
+                        connection.sendall(str(control_number).encode()) # Send control_number back to the client
+                    else:
+                        connection.sendall(data)  # Echo back the received data
                 else:
-                    connection.sendall(data)  # Echo back the received data
-            else:
-                break
-    finally:
-        connection.close()
+                    break
+        finally:
+            connection.close()
+finally:
+    server_socket.close()
