229f7d86e7bf2da514fdceba467cc1e499265580
Nikolas Doan <hoangphuongbluesky@gmail.com>
2024-10-15 21:32:55 +0800

    2nd commit on oliver

--- test_server.py
+++ test_server.py
@@ -1,6 +1,5 @@
+# server.py
 import socket
-import signal
-import sys
 from gpt import run_gpt
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
@@ -9,43 +8,31 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address and port
-server_address = ('192.168.1.87', 6666)  # Listen on all interfaces
+server_address = ('0.0.0.0', 5000)  # Listen on all interfaces
 server_socket.bind(server_address)
 
 # Listen for incoming connections
 server_socket.listen(1)
-print("Server is listening on port 6666...")
+print("Server is listening on port 5000...")
 
-def signal_handler(sig, frame):
-    print("Interrupt received, shutting down...")
-    server_socket.close()
-    sys.exit(0)
+while True:
+    # Wait for a connection
+    connection, client_address = server_socket.accept()
+    try:
+        print(f"Connection from {client_address}")
 
-# Register the signal handler for graceful shutdown
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
-
-try:
-    while True:
-        # Wait for a connection
-        connection, client_address = server_socket.accept()
-        try:
-            print(f"Connection from {client_address}")
-
-            while True:
-                # Receive data from the client
-                data = connection.recv(1024)
-                if data:
-                    received_text = data.decode()
-                    print(f"Received: {received_text}")
-                    if received_text == 'start':  # Replace 'start' with the specific character or string you want
-                        control_number = run_gpt()
-                        connection.sendall(str(control_number).encode()) # Send control_number back to the client
-                    else:
-                        connection.sendall(data)  # Echo back the received data
+        while True:
+            # Receive data from the client
+            data = connection.recv(1024)
+            if data:
+                received_text = data.decode()
+                print(f"Received: {received_text}")
+                if received_text == 'start':  # Replace 'start' with the specific character or string you want
+                    control_number = run_gpt()
+                    connection.sendall(str(control_number).encode()) # Send control_number back to the client
                 else:
-                    break
-        finally:
-            connection.close()
-finally:
-    server_socket.close()
+                    connection.sendall(data)  # Echo back the received data
+            else:
+                break
+    finally:
+        connection.close()
